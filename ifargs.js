8ef9d6396b138a4ba2eb43278bdfd2031a62fbff
mikkotikkanen <mikko.tikkanen@gmail.com>
2014-02-14 16:42:23 +0200

    Added ... array handling to filter

--- ifargs.js
+++ ifargs.js
@@ -1,7 +1,7 @@
 /*!
  * ifargs
  * 
- * Making complex argumenthandling simple.
+ * Simplified handling of high entropy arguments
  * 
  * @version 1.0
  * @author Mikko Tikkanen <mikko.tikkanen@gmail.com>
@@ -10,6 +10,7 @@
 	var O = {};
 	
   	var fnc = function(args, filter) {
+		args = [].slice.call(args, 0);
 		var vars = {};
 		
 		// Make sure filters match
@@ -21,6 +22,11 @@
 			
 			if(typeof args[i] != item[2]) { return; }
 			vars[item[1]] = args[i];
+		}
+		
+		//if(args.lenth != vars.length && /\.\.\.$/.test(filter[filter.length-1])) {
+		if(/\.\.\.$/.test(filter[filter.length-1])) {
+			vars[item[1]] = args.slice(filter.length-1);
 		}
 		
 		return vars;
