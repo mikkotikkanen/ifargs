b8e13cf6e2804287b1b0e5728c9fa8e6498a49e8
Mikko Tikkanen <mikko.tikkanen@gmail.com>
2014-03-17 18:56:03 +0200

    Fixed optional arguments

--- ifargs.js
+++ ifargs.js
@@ -9,36 +9,42 @@
 /* jshint browser: true, devel: true */
 ;(function(window, document, undefined) {
 	var O = {};
-	
+
 	var fnc = function(args, filter) {
 		args = [].slice.call(args, 0);
 		var vars = {};
-		
+
 		// Make sure filters match
 		filter = filter.split(',');
 		var item;
-		for(var i = 0; i < filter.length; i++) {
-			item = filter[i].replace(/^[\s]+|[\s\.]+$/gi, ''); // Trim (whitespace + ".")
-			item = item.match(/([\w_]+)\[?([\w_]*)\]?/);
-			
+		for(var i = 0, j = 0; i < filter.length; i++, j++) {
+			item = filter[i].replace(/^[\s]+|[\s\.]+$/gi, ''); // Trim (whitespace & ".")
+			item = item.match(/([\w_]+)\[?([\w_]*)\]?/); // Split item to name(1) & possible type(2)
+
 			if(!item) { return null; } // Match failed
-			if(!args[i] && !/\?$/.test(filter[i])) { return null; } // Argument non-existing and not optional
-			if(item[2] && typeof args[i] != item[2] && !/\?$/.test(filter[i])) { return null; } // Item type defined & type differs & not optional
-			
-			vars[item[1]] = args[i];
+			if(!args[j] && !/\?$/.test(filter[i])) { return null; } // Argument non-existing and not optional
+			if(item[2] && typeof args[j] != item[2]) { // Item type defined & differs
+				if(/\?$/.test(filter[i])) { // If it was optional, and rewind argument selector and move to next filter
+					j--;
+					continue;
+				} else  {
+					return null;
+				}
+			}
+
+			vars[item[1]] = args[j];
 		}
-		
+
 		// Handle argument arrays (...)
 		if(args.length >= filter.length && /\.\.\.$/.test(filter[filter.length-1])) {
 			vars[item[1]] = args.slice(filter.length-1);
 		}
-		
+
 		return vars;
 	};
-	
-	
+
+
 	// Expose to public
 	window.ifargs = fnc;
-	
-})(window, document);
 
+})(window, document);
